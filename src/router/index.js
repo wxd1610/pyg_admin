e81ce399f18b40250b3fe51ec441bcddbaa165a1
wxd1610 <1581401610@qq.com>
2019-04-20 21:06:29 +0800

    登陆后进入首页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+// import VueRouter from 'vue-router';
 
 Vue.use(Router)
 
@@ -8,8 +10,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 })
+// 添加导航守卫
+// router.beforeEach((to, from, next) => {
+//   // to去的路由对象
+//   // from离开路由对象
+//   // next滴啊用 下一个路由
+//   // next('/xxx')去某个路由
+//   if (to.path === '/login') return next()
+
+//   if (!sessionStorage.getItem('token')) return next('/login')
+//   next()
+// })
